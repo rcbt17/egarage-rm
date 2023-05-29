0c54a1fd56f0aad70803d3e3b7ed29d123a05e03
matteo argenio <matteargy@gmail.com>
2023-05-29 14:25:05 +0200

    doing view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_094952) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_121730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_094952) do
     t.integer "seats"
     t.boolean "air_conditioning"
     t.integer "price_per_day"
-    t.string "class"
+    t.string "class_type"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
